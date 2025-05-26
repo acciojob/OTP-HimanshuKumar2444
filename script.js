dd0cb6ea5915f55e6f2938ff8b36f13a54e8510c
HimanshuKumar2971 <144801042+HimanshuKumar2444@users.noreply.github.com>
2025-05-27 00:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (/^\d$/.test(value)) {
+      // Move to next input if exists
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      input.value = ""; // Only allow digits
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = "";
+        }
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
